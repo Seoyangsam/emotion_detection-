3be9477ed4a9837a14712d44a5b5cf0accbb88cd
seoyangsam <robert.xu@wur.nl>
2023-02-10 18:14:55 +0100

    initial commit

--- CNN.py
+++ CNN.py
@@ -62,20 +62,20 @@ train_generator = train_datagen.flow_from_directory(
         train_dir,
         target_size=(128, 128),
         batch_size=batch_size,
-        color_mode="grayscale",
+        color_mode="rgb",
         class_mode='categorical')
 
 validation_generator = val_datagen.flow_from_directory(
         val_dir,
         target_size=(128, 128),
         batch_size=batch_size,
-        color_mode="grayscale",
+        color_mode="rgb",
         class_mode='categorical')
 
 # Create the model
 model = Sequential()
 
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(128, 128, 1)))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(128, 128, 3)))
 model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
@@ -121,16 +121,18 @@ def train(mode):
             if not ret:
                 break
             facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+            #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = facecasc.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=5)
 
             for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
-                roi_gray = gray[y:y + h, x:x + w]
-                cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (128, 128)), -1), 0)
+                cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
+                roi_gray = frame[y:y + h, x:x + w]
+                cropped_img = np.expand_dims(cv2.resize(roi_gray, (128, 128)), 0)
                 prediction = model.predict(cropped_img)
                 maxindex = int(np.argmax(prediction))
-                cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                cv2.putText(frame, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                            (255, 255, 255), 2, cv2.LINE_AA)
+
 
             cv2.imshow('Video', cv2.resize(frame, (1600, 960), interpolation=cv2.INTER_CUBIC))
             if cv2.waitKey(1) & 0xFF == ord('x'):
